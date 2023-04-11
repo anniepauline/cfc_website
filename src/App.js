99d8f98f02fb3cd459547c21ac46738276966c3a
Annie Pauline <anniepauline.23@gmail.com>
2023-04-11 18:43:40 +0530

    final changes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Footer from "./components/footer/Footer.jsx";
 import Card from "./components/card/Card.jsx";
 import Buttons from "./components/buttons/Button.jsx";
 import Speakers from "./components/speakers/Speakers.jsx";
+import { ReactComponent as Line } from "./icons/Line.svg";
 import './app.css';
 
 function App() {   
@@ -11,8 +12,8 @@ function App() {
         <div id = "title">
           <p >Zac Poonen</p>
           <p >ANSWERS</p>
-          <hr class = "line"/>
-          <p >Listen to Zac Poonen’s answers to over 600 questions, which is a treasure house of wisdom.</p>
+          <Line id = "line"/>
+          <p class = "p-desc">Listen to Zac Poonen’s answers to over 600 questions, which is a treasure house of wisdom.</p>
         </div>
         <img src = "https://www.cfcindia.com/resources/en/images/app/br-zac-right-card.png" alt = "main-img"/>
       </div>
